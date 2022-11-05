a0a59321af4db5c4ae5a703c054f8bad90956ab2
= <=>
2022-11-05 16:52:23 -0300

    creating class based views'

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     path('new_review', new_review, name='new_review'),
     path('list_reviews', list_reviews, name='list_reviews'),
+    path('new_review_v2', NewReview.as_view(), name='new_review_v2'), 
+    path('list_reviews_v2', ListReviews.as_view(), name='list_reviews_v2')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
-
+from django.views.generic import ListView, CreateView
 
 # Create your views here.
 
@@ -10,12 +10,24 @@ def new_review(request):
         form = BookReviewForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse('Salvo!')
+            return redirect('list_reviews')
     else:
         form = BookReviewForm()
     return render(request, 'book_review_form.html', {'form': form})
 
 
+class NewReview(CreateView):
+    model = BookReview
+    form_class =  BookReviewForm
+    template_name = 'book_review_form.html'
+    success_url = 'list_reviews'
+
+
 def list_reviews(request):
-    reviews = BookReview.objects.all().order_by('title')
-    return render(request, 'book_review_list.html', {'reviews': reviews})
+    object_list = BookReview.objects.all().order_by('title')
+    return render(request, 'book_review_list.html', {'object_list': object_list})
+
+
+class ListReviews(ListView):
+    queryset = BookReview.objects.all().order_by('title')
+    template_name = 'book_review_list.html'
